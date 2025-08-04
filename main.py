3b7228418b436e249f94b8a1fbd88e28e5b6058f
Cantillo <bryan.cantillo@accenture.com>
2025-08-04 15:54:34 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+inventory = [
+    { "Product": "Microwave", "SKU": "MW837291" },
+    { "Product": "Microwave", "SKU": "MW102938" },
+    { "Product": "Microwave", "SKU": "MW564738" },
+    { "Product": "Refrigerator", "SKU": "RF384729" },
+    { "Product": "Refrigerator", "SKU": "RF927384" },
+    { "Product": "Refrigerator", "SKU": "RF102983" },
+    { "Product": "Washing Machine", "SKU": "WM837492" },
+    { "Product": "Washing Machine", "SKU": "WM293847" },
+    { "Product": "Washing Machine", "SKU": "WM928374" },
+    { "Product": "Dryer", "SKU": "DR102938" },
+    { "Product": "Dryer", "SKU": "DR837465" },
+    { "Product": "Dryer", "SKU": "DR564738" },
+    { "Product": "Dishwasher", "SKU": "DW837192" },
+    { "Product": "Dishwasher", "SKU": "DW182736" },
+    { "Product": "Dishwasher", "SKU": "DW918273" }
+]
+
+@app.get("/products")
+def get_products():
+    return JSONResponse(content=inventory)
